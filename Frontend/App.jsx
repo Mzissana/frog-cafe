12f5769bb2d25923db6453d274c7e99971a58d5c
Mzissana <kdk459915@hotmail.com>
2025-06-09 18:46:43 +0300

    Исправляет ошибки с router

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
   return (
     <AuthProvider>
       <CartProvider>
-        <Router>
+        <Router basename="/frog-cafe/">
           <Routes>
             <Route path="/login" element={<Login />} />
 
